8220f796088fe283b05cf7cf8422a30b96ba0be4
kakakaka04 <119366427+kakakaka04@users.noreply.github.com>
2024-03-03 17:03:06 -0700

    Configure DB & Redis variables

--- config/database.php
+++ config/database.php
@@ -138,12 +138,13 @@ return [
         ],
 
         'cache' => [
-            'scheme' => 'tls'
+            'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
-            'username' => env('AZURE_REDIS_USERNAME'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
+            'scheme' => 'tls'
         ],
 
     ],
